7a050d0226eb5f86ce166f02c4c6d1d10edd8344
yeeun <jovm268@naver.com>
2025-10-21 14:46:37 +0900

    v1.2

--- app.js
+++ app.js
@@ -1,78 +1,81 @@
-import fs from 'node:fs/promises';
-import bodyParser from 'body-parser';
-import express from 'express';
+import bodyParser from "body-parser";
+import express from "express";
+import fs from "node:fs/promises";
 
 const app = express();
 
-app.use(express.static('images'));
+app.use(express.static("images"));
 app.use(bodyParser.json());
 
 // CORS
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // allow all domains
-  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Origin", "*"); // allow all domains
+  res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   next();
 });
 
-app.get('/places', async (req, res) => {
-  const fileContent = await fs.readFile('./data/places.json');
+app.get("/places", async (req, res) => {
+  const fileContent = await fs.readFile("./data/places.json");
 
   const placesData = JSON.parse(fileContent);
 
   res.status(200).json({ places: placesData });
 });
 
-app.get('/users/places', async (req, res) => {
-  const fileContent = await fs.readFile('./data/user-places.json');
+app.get("/users/places", async (req, res) => {
+  const fileContent = await fs.readFile("./data/user-places.json");
 
   const places = JSON.parse(fileContent);
 
   res.status(200).json({ places });
 });
 
-app.post('/users/places', async (req, res) => {
+app.post("/users/places", async (req, res) => {
   const newPlace = req.body.place;
 
-  const fileContent = await fs.readFile('./data/user-places.json');
+  const fileContent = await fs.readFile("./data/user-places.json");
   const userPlaces = JSON.parse(fileContent);
 
   // Add or update place
-  const existingIndex = userPlaces.findIndex((place) => place.id === newPlace.id);
+  const existingIndex = userPlaces.findIndex(
+    (place) => place.id === newPlace.id
+  );
   if (existingIndex >= 0) {
     userPlaces[existingIndex] = newPlace; // Update existing place
   } else {
     userPlaces.push(newPlace); // Add new place
   }
 
-  await fs.writeFile('./data/user-places.json', JSON.stringify(userPlaces));
+  await fs.writeFile("./data/user-places.json", JSON.stringify(userPlaces));
 
-  res.status(200).json({ message: 'User place added/updated!' });
+  res.status(200).json({ message: "User place added/updated!" });
 });
 
-
-app.delete('/users/places/:id', async (req, res) => {
+app.delete("/users/places/:id", async (req, res) => {
   const placeId = req.params.id;
 
-  const fileContent = await fs.readFile('./data/user-places.json');
+  const fileContent = await fs.readFile("./data/user-places.json");
   const userPlaces = JSON.parse(fileContent);
 
   // Remove place by ID
   const updatedPlaces = userPlaces.filter((place) => place.id !== placeId);
 
-  await fs.writeFile('./data/user-places.json', JSON.stringify(updatedPlaces));
+  await fs.writeFile("./data/user-places.json", JSON.stringify(updatedPlaces));
 
   res.status(204).send();
 });
 
 // 404
 app.use((req, res, next) => {
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     return next();
   }
-  res.status(404).json({ message: '404 - Not Found' });
+  res.status(404).json({ message: "404 - Not Found" });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
